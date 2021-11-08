a5a3c0442eaf48848faa921ee016a583ee869299
unknown <nourguerfali08@gmail.com>
2021-11-08 21:19:21 +0100

    Page-NotFound

--- src/app/app--routing.module.ts
+++ src/app/app--routing.module.ts
@@ -3,14 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { TableComponent} from './table/table.component';
 import { ContenuComponent } from './contenu/contenu.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
 
 const routes: Routes = [
 { path: 'first-component', component:TableComponent },
 { path: '', component:ContenuComponent},
+{ path: '**', component:NotFoundComponent},
 ];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  
+    NotFoundComponent
+  ],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
