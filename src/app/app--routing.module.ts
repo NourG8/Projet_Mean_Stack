2010e4f45219854fc5cc87d122e1733de2177638
unknown <nourguerfali08@gmail.com>
2021-11-08 18:56:38 +0100

    second commit

--- src/app/app--routing.module.ts
+++ src/app/app--routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { TableComponent} from './table/table.component';
+import { ContenuComponent } from './contenu/contenu.component';
 
 const routes: Routes = [
 { path: 'first-component', component:TableComponent },
+{ path: '', component:ContenuComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TableComponent } from './table/table.component';
 import { AppRoutingModule } from './app--routing.module';
-
+import { ContenuComponent } from './contenu/contenu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent
+    TableComponent,
+    ContenuComponent
   ],
   imports: [
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
